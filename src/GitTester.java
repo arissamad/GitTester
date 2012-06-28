f3e9187b5c4ae6c7ac959c498e621a2e7e998616
Aris Samad <aris@quickschools.com>
2012-06-28 18:06:58 -0400

    3rd version.

--- src/GitTester.java
+++ src/GitTester.java
@@ -1,7 +1,7 @@
 
 public class GitTester {
 	public static void main(String[] args) {
-		System.out.println("GitTest start.");
+		System.out.println("GitTest start. v2.");
 		System.out.println("End");
 	}
 }
