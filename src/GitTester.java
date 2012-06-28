29702f17b8902d97b8faff64d660350feff4ba49
Aris Samad <aris@quickschools.com>
2012-06-28 18:07:14 -0400

    fourth version.

--- src/GitTester.java
+++ src/GitTester.java
@@ -1,7 +1,7 @@
 
 public class GitTester {
 	public static void main(String[] args) {
-		System.out.println("GitTest start. v2.");
+		System.out.println("GitTest start. v4.");
 		System.out.println("End");
 	}
 }
